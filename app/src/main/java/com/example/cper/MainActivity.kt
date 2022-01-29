f37914f13fbead4de60254c7bd3e720668eb7475
blayush <ayushmishra42@gmail.com>
2022-01-29 20:11:57 +0530

    add- single view for recyclerView

--- app/src/main/java/com/example/cper/MainActivity.kt
+++ app/src/main/java/com/example/cper/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.cper
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+//        requestWindowFeature(Window.FEATURE_NO_TITLE)
+//        window.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
         setContentView(R.layout.activity_main)
+        supportActionBar?.hide()
+
     }
 }
