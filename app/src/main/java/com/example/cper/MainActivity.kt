245e7404390a51723eb0d91c5c3673168682ee88
blayush <ayushmishra42@gmail.com>
2022-01-31 22:39:58 +0530

    Added : better UI, days to go, Date UTC to IST, Square cardView Contest Icon

--- app/src/main/java/com/example/cper/MainActivity.kt
+++ app/src/main/java/com/example/cper/MainActivity.kt
@@ -15,25 +15,13 @@ import com.google.gson.GsonBuilder
 
 class MainActivity : AppCompatActivity() {
     private lateinit var recView: RecyclerView
-    var url = "https://kontests.net/api/v1/codeforces"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 //        requestWindowFeature(Window.FEATURE_NO_TITLE)
 //        window.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
         setContentView(R.layout.activity_main)
         supportActionBar?.hide()
-        // add dialog box soon for details of the platform
 
-//        recView.findViewById<RecyclerView>(R.id.recyclerView)
-//        recView.layoutManager = LinearLayoutManager(this)
-//        var requestQueue = Volley.newRequestQueue(this)
-//        val stringRequest =
-//            StringRequest(Request.Method.GET, url, Response.Listener<String> { response ->
-//                var gsonBuilder: GsonBuilder
-//                var gson: Gson
-//                RecyclerViewDatum[] data = gson . fromJson (response, RecyclerViewDatum.class)
-//
-//            }, Response.ErrorListener { Toast.makeText(this, "Error", LENGTH_LONG) })
-//        requestQueue.add(stringRequest)
     }
 }
